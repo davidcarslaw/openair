fc2533e4c42a8085002baef82bb29b780e31d7a8
DCCKC <david.carslaw@kcl.ac.uk>
2015-07-17 12:00:12 +0100

    fix weighting issues

--- R/timeProp.R
+++ R/timeProp.R
@@ -213,33 +213,35 @@ timeProp <- function(mydata, pollutant = "nox", proportion = "cluster", avg.time
         ## the values
         values <- plyr::ddply(mydata, proportion, timeAverage, avg.time = avg.time,
                               statistic = "mean")
-
+        
         ## do not weight by concentration if statistic = frequency, just repeat overall mean
         ## by proportion
         if (statistic == "frequency") {
-
+            ## scales frequencies by daily mean
             tmp <- timeAverage(mydata, avg.time)
-            values[, pollutant] <- rep(tmp[, pollutant], length(unique(mydata[, proportion])))
-        }
+            tmp$means <- tmp[[pollutant]]
 
+            values <- merge(values, tmp[c("date", "means")], by = "date", all = TRUE)
+            values <- sortDataFrame(values, key = c(proportion, "date"))
+        }
+        
         ## add frequencies
         values$freq <- freqs[[pollutant]]
 
-        ## means
-  #      values$means <- values[[pollutant]]
-
-        ## conc * freq
+        ## conc * freq weighting
         values$sums <- freqs[[pollutant]] * values[[pollutant]]
 
-     #   if (statistic == "mean") {
+        if (statistic == "mean") {
             ## weighted conc
-            res <- plyr::ddply(values, .(date), transform, Var1 = sums / sum(freq, na.rm = TRUE))
+            res <- plyr::ddply(values, .(date), transform,
+                               Var1 = sums / sum(freq, na.rm = TRUE))
             
-     #   } else {
+        } else {
 
-       #     res <- plyr::ddply(values, .(date), transform, Var1 = means * freq / sum(freq, na.rm = TRUE))
+            res <- plyr::ddply(values, .(date), transform,
+                               Var1 = means * freq / sum(freq, na.rm = TRUE))
             
-    #    }
+        }
 
         ## normlaise to 100 if needed
         if (normalise) res <- plyr::ddply(res, .(date), transform,
