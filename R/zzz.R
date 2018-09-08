5df84aa0bc2610f2b873cb6bf1d53aef1f6ffad1
davidcarslaw <david.carslaw@york.ac.uk>
2018-09-08 12:16:30 +0100

    bump version ready for CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,6 @@
   ##  packageStartupMessage("\ttype citation(\"openair\") for how to cite openair")
   utils::globalVariables(c("variable", "value", "res", "site_pol",
                            ".", "data", "fit", "grp", "intercept", "out",
-                           "seslope", "slope", "weekday", "cuts", "empty"))
+                           "seslope", "slope", "weekday", "cuts", "empty",
+                           "lat", "lon", "traj_len", "wkday"))
 }
