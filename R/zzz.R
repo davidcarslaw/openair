fb64c28e261f99d36f34518bce607505c054ad9e
davidcarslaw <david.carslaw@york.ac.uk>
2019-09-24 09:38:12 +0100

    add option to pivot AURN data + return astibble

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(...) {
 
   ##  packageStartupMessage("\ttype citation(\"openair\") for how to cite openair")
-  utils::globalVariables(c("variable", "value", "res", "site_pol",
+  utils::globalVariables(c("variable", "value", "res", "site_pol", "code",
                            ".", "data", "fit", "grp", "intercept", "out",
                            "seslope", "slope", "weekday", "cuts", "empty",
                            "lat", "lon", "traj_len", "wkday"))
