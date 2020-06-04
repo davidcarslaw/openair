bb142bbe1e99419e57b3306b2ee1424bd8ed26c7
davidcarslaw <david.carslaw@york.ac.uk>
2020-06-04 21:53:32 +0100

    keep check happy

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 
   ##  packageStartupMessage("\ttype citation(\"openair\") for how to cite openair")
   utils::globalVariables(c("variable", "value", "res", "site_pol", "code",
-                           "latitude", "longitude", "site.type",
+                           "latitude", "longitude", "site.type", "temp",
                            ".", "data", "fit", "grp", "intercept", "out",
                            "seslope", "slope", "weekday", "cuts", "empty",
                            "lat", "lon", "traj_len", "wkday", "date_end",
