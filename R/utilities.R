bbdee60db516bb4867a3c5e7322fffc347bfb461
davidcarslaw <david.carslaw@york.ac.uk>
2021-03-16 10:18:29 +0000

    fix case with missing before/after when difference = TRUE

--- R/utilities.R
+++ R/utilities.R
@@ -868,7 +868,7 @@ bootMeanDiff <- function(mydata, x = "x", y = "y", conf.int = 0.95, B = 1000) {
   y <- na.omit(mydata[[y]])
   Mean <- mean(y) - mean(x)
 
-  if (nrow(mydata) < 2) {
+  if (nrow(mydata) < 2 | is.na(Mean)) {
     res1 <- data.frame(variable = x.name, Mean = mean(x), Lower = NA, Upper = NA,
                        stringsAsFactors = FALSE)
     
