c4b7bb2a8423300f2c234b8bf577ac00c5c65de2
davidcarslaw <david.carslaw@york.ac.uk>
2022-06-15 15:45:03 +0100

    remove unneeded code

--- R/polarPlot.R
+++ R/polarPlot.R
@@ -1655,14 +1655,3 @@ interp.surface <- function(obj, loc) {
                                                                                                                      ex) * ey + z[cbind(lx1 + 1, ly1 + 1)] * ex * ey)
 }
 
-interp.surface.grid <- function(obj, grid.list) {
-  x <- grid.list$x
-  y <- grid.list$y
-  M <- length(x)
-  N <- length(y)
-  out <- matrix(NA, nrow = M, ncol = N)
-  for (i in 1:M) {
-    out[i, ] <- interp.surface(obj, cbind(rep(x[i], N), y))
-  }
-  list(x = x, y = y, z = out)
-}
