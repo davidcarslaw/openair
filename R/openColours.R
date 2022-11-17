9e3c93a1abdef57bf9d2db7f804e43ae3a554d89
Jack Davison <davison.jack.jd@gmail.com>
2022-11-17 09:23:15 +0000

    feat (openColours.R): add "turbo"
    Add the turbo colour palette, which was designed by Google to be an improvement over "jet".

--- R/openColours.R
+++ R/openColours.R
@@ -8,9 +8,9 @@
 ##'
 ##' Each of the pre-defined schemes have merits and their use will depend on a
 ##' particular situation. For showing incrementing concentrations e.g. high
-##' concentrations emphasised, then "default", "heat", "jet" and "increment" are
-##' very useful. See also the description of \code{RColorBrewer} schemes for the
-##' option \code{scheme}.
+##' concentrations emphasised, then "default", "heat", "jet", "turbo", and
+##' "increment" are very useful. See also the description of \code{RColorBrewer}
+##' schemes for the option \code{scheme}.
 ##'
 ##' To colour-code categorical-type problems e.g. colours for different
 ##' pollutants, "hue" and "brewer1" are useful.
@@ -24,12 +24,12 @@
 ##' To see the full list of names, type \code{colors()} into R.
 ##'
 ##' @param scheme The pre-defined schemes are "increment", "default", "brewer1",
-##'   "heat", "jet", "hue", "greyscale", or a vector of R colour names e.g.
-##'   c("green", "blue"). It is also possible to supply colour schemes from the
-##'   \code{RColorBrewer} package. This package defines three types of colour
-##'   schemes: sequential, diverging or qualitative. See
-##'   \url{https://colorbrewer2.org/} for more details concerning the orginal work
-##'   on which this is based.
+##'   "heat", "jet", "turbo", "hue", "greyscale", or a vector of R colour names
+##'   e.g. c("green", "blue"). It is also possible to supply colour schemes from
+##'   the \code{RColorBrewer} package. This package defines three types of
+##'   colour schemes: sequential, diverging or qualitative. See
+##'   \url{https://colorbrewer2.org/} for more details concerning the orginal
+##'   work on which this is based.
 ##'
 ##'   Simplified versions of the \code{viridis} colours are also available.
 ##'   These include "viridis", "plasma", "magma", "inferno" and "cividis".
@@ -74,7 +74,7 @@
 ##' cols <- openColours(c("yellow", "green", "red"), 10)
 ##' cols
 ##'
-##' 
+##'
 openColours <- function(scheme = "default", n = 100) {
 
   ## pre-defined brewer colour palletes sequential, diverging, qualitative
@@ -89,48 +89,51 @@ openColours <- function(scheme = "default", n = 100) {
   brewer.n <- c(rep(9, 18), rep(9, 9), c(8, 8, 12, 9, 8, 9, 8, 12))
 
   ## predefined schemes
-  schemes <- c("increment", "default", "brewer1", "heat", "jet", "hue", 
+  schemes <- c("increment", "default", "brewer1", "heat", "jet", "hue",
                "greyscale", brewer.col, "cbPalette", "viridis", "magma",
-               "inferno", "plasma", "cividis")
+               "inferno", "plasma", "cividis", "turbo")
 
 
   ## schemes
   heat <- colorRampPalette(brewer.pal(9, "YlOrRd"), interpolate = "spline")
 
   viridis <- colorRampPalette(c(
-    "#440154FF", "#482878FF", "#3E4A89FF", "#31688EFF", "#26828EFF", "#1F9E89FF", 
+    "#440154FF", "#482878FF", "#3E4A89FF", "#31688EFF", "#26828EFF", "#1F9E89FF",
     "#35B779FF", "#6DCD59FF", "#B4DE2CFF", "#FDE725FF"
   ))
-  
+
   inferno <- colorRampPalette(c(
     "#000004FF", "#1B0C42FF", "#4B0C6BFF", "#781C6DFF", "#A52C60FF", "#CF4446FF",
     "#ED6925FF", "#FB9A06FF", "#F7D03CFF", "#FCFFA4FF"
   ))
-  
+
   magma <- colorRampPalette(c(
     "#000004FF", "#180F3EFF", "#451077FF", "#721F81FF", "#9F2F7FFF", "#CD4071FF",
     "#F1605DFF", "#FD9567FF", "#FEC98DFF", "#FCFDBFFF"
   ))
-  
+
   plasma <- colorRampPalette(c(
     "#0D0887FF", "#47039FFF", "#7301A8FF", "#9C179EFF", "#BD3786FF", "#D8576BFF",
     "#ED7953FF", "#FA9E3BFF", "#FDC926FF", "#F0F921FF"
   ))
-  
+
   cividis <- colorRampPalette(c(
     "#00204DFF", "#00336FFF", "#39486BFF", "#575C6DFF", "#707173FF", "#8A8779FF",
     "#A69D75FF", "#C4B56CFF", "#E4CF5BFF", "#FFEA46FF"
   ))
-  
-  
+
+
   jet <- colorRampPalette(c(
     "#00007F", "blue", "#007FFF", "cyan",
     "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"
   ))
 
+  turbo <- colorRampPalette(c(
+    '#30123BFF', '#4662D7FF', '#36AAF9FF', '#1AE4B6FF', '#72FE5EFF',
+    '#C7EF34FF', '#FABA39FF', '#F66B19FF', '#CB2A04FF', '#7A0403FF'
+  ))
+
   default.col <- colorRampPalette(brewer.pal(11, "Spectral"), interpolate = "spline")
-
-
 
   ## for this pallete use specfified number if possible - because it has been thought about...
   brewer1 <- function(n) {
@@ -178,22 +181,22 @@ openColours <- function(scheme = "default", n = 100) {
 
   # The palette with grey:
   cbPalette <- function(n) {
-    
-    cols <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", 
+
+    cols <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
               "#D55E00", "#CC79A7")
-    
+
     if (n >= 1 && n < 9) {
-      
-      cols <- cols[1:n] 
-      
+
+      cols <- cols[1:n]
+
     } else {
-    
+
       warning("Too many colours selected. Should be 1 to 8.")
     }
   }
-    
-  
-  
+
+
+
   ## error catcher
   if (length(scheme) == 1) {
     if (scheme %in% brewer.col) cols <- find.brewer(scheme, n)
@@ -203,6 +206,7 @@ openColours <- function(scheme = "default", n = 100) {
     if (scheme %in% brewer.col) cols <- find.brewer(scheme, n)
     if (scheme == "heat") cols <- heat(n)
     if (scheme == "jet") cols <- jet(n)
+    if (scheme == "turbo") cols <- turbo(n)
     if (scheme == "viridis") cols <- viridis(n)
     if (scheme == "magma") cols <- magma(n)
     if (scheme == "inferno") cols <- inferno(n)
