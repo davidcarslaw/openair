37768ea9972b776395b39f6f9844aa2f770f3f3d
Jack Davison <davison.jack.jd@gmail.com>
2022-12-12 11:45:46 +0000

    docs (R/mydata.R): remove "list"
    Nowadays you don't need list() which just ends up printing strangely in the help pane / pkgdown site.

--- R/mydata.R
+++ R/mydata.R
@@ -1,45 +1,40 @@
-##' Example data for openair
-##'
-##' The mydata dataset is provided as an example dataset as part of the openair
-##' package. The dataset contains hourly measurements of air pollutant
-##' concentrations, wind speed and wind direction collected at the Marylebone
-##' (London) air quality monitoring supersite between 1st January 1998 and 23rd
-##' June 2005. The data set is a tibble.
-##'
-##' \code{mydata} is supplied with the \code{openair} package as an example
-##' dataset for use with documented examples.
-##'
-##' @name mydata
-##' @docType data
-##' @format Data frame with 65533 observations (rows) on the following 10
-##'   variables: \describe{ \item{list("date")}{Observation date/time stamp in
-##'   year-month-day hour:minute:second format (POSIXct). }
-##'   \item{list("ws")}{Wind speed, in m/s, as numeric vector.}
-##'   \item{list("wd")}{Wind direction, in degrees from North, as a numeric
-##'   vector.} \item{list("nox")}{Oxides of nitrogen concentration, in ppb, as
-##'   a numeric vector.} \item{list("no2")}{Nitrogen dioxide concentration, in
-##'   ppb, as a numeric vector.} \item{list("o3")}{Ozone concentration, in ppb,
-##'   as a numeric vector.} \item{list("pm10")}{Particulate PM10 fraction
-##'   measurement, in ug/m3 (raw TEOM), as a numeric vector.}
-##'   \item{list("so2")}{Sulfur dioxide concentration, in ppb, as a numeric
-##'   vector.} \item{list("co")}{Carbon monoxide concentration, in ppm, as a
-##'   numeric vector.} \item{list("pm25")}{Particulate PM2.5 fraction
-##'   measurement, in ug/m3, as a numeric vector.} }
-##' @note \code{openair} functions generally require data frames with a field
-##'   "date" that can be in either \code{POSIXct} or \code{Date} format but
-##'   should be GMT time zone. This can be hourly data or higher resolution
-##'   data.
-##' @source \code{mydata} was compiled from data archived in the London Air
-##'   Quality Archive.  See \url{https://www.londonair.org.uk} for site details.
-##'
-##' The same data is also provide in \code{'.csv'} format via the openair
-##'   project web site \url{https://davidcarslaw.github.io/openair/}.
-##' @keywords datasets
-##' @examples
-##'
-##'
-##' #basic structure
-##' head(mydata)
-##'
-##'
-NULL
+#' Example data for openair
+#'
+#' The mydata dataset is provided as an example dataset as part of the openair
+#' package. The dataset contains hourly measurements of air pollutant
+#' concentrations, wind speed and wind direction collected at the Marylebone
+#' (London) air quality monitoring supersite between 1st January 1998 and 23rd
+#' June 2005. The data set is a tibble.
+#'
+#' \code{mydata} is supplied with the \code{openair} package as an example
+#' dataset for use with documented examples.
+#'
+#' @name mydata
+#' @docType data
+#' @format Data frame with 65533 observations (rows) and 10 variables:
+#' \describe{
+#' \item{date}{Observation date/time stamp in year-month-day hour:minute:second
+#' format (POSIXct).}
+#' \item{ws}{Wind speed, in m/s, as numeric vector.}
+#' \item{wd}{Wind direction, in degrees from North, as a numeric vector.}
+#' \item{nox}{Oxides of nitrogen concentration, in ppb, as a numeric vector.}
+#' \item{no2}{Nitrogen dioxide concentration, in ppb, as a numeric vector.}
+#' \item{o3}{Ozone concentration, in ppb, as a numeric vector.}
+#' \item{pm10}{Particulate PM10 fraction measurement, in ug/m3 (raw TEOM), as a
+#' numeric vector.}
+#' \item{so2}{Sulfur dioxide concentration, in ppb, as a numeric vector.}
+#' \item{co}{Carbon monoxide concentration, in ppm, as a numeric vector.}
+#' \item{pm25}{Particulate PM2.5 fraction measurement, in ug/m3, as a numeric
+#' vector.}
+#' }
+#' @note \code{openair} functions generally require data frames with a field
+#'   "date" that can be in either \code{POSIXct} or \code{Date} format but
+#'   should be GMT time zone. This can be hourly data or higher resolution
+#'   data.
+#' @source \code{mydata} was compiled from data archived in the London Air
+#'   Quality Archive.  See \url{https://www.londonair.org.uk} for site details.
+#' @keywords datasets
+#' @examples
+#' #basic structure
+#' head(mydata)
+"mydata"
