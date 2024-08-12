0f5337139fd2253965d6e4aee508cde718767d22
jack-davison <davison.jack.jd@gmail.com>
2024-08-12 08:28:27 +0100

    fix: use magrittr pipe

--- R/importUKAQ-utils.R
+++ R/importUKAQ-utils.R
@@ -414,19 +414,19 @@ filter_site_pollutant <- function(aq_data, site, pollutant, to_narrow, data_type
 #' @param site Sites passed to [importUKAQ()]
 #' @noRd
 guess_source <- function(site) {
-  ukaq_meta <- importMeta("ukaq") |>
-    dplyr::mutate(source = factor(.data$source, c("aurn", "saqn", "aqe", "waqn", "ni", "local"))) |>
-    dplyr::arrange(.data$source) |>
+  ukaq_meta <- importMeta("ukaq") %>%
+    dplyr::mutate(source = factor(.data$source, c("aurn", "saqn", "aqe", "waqn", "ni", "local"))) %>%
+    dplyr::arrange(.data$source) %>%
     dplyr::distinct(.data$site, .data$latitude, .data$longitude, .keep_all = TRUE)
   
   source_tbl <-
-    data.frame(code = toupper(site)) |>
+    data.frame(code = toupper(site)) %>%
     dplyr::left_join(ukaq_meta, by = "code")
 
   if (any(is.na(source_tbl$source))) {
     ambiguous_codes <-
-      source_tbl |> 
-      dplyr::filter(is.na(.data$source)) |>
+      source_tbl %>% 
+      dplyr::filter(is.na(.data$source)) %>%
       dplyr::pull(.data$code)
 
     cli::cli_abort(
@@ -439,12 +439,12 @@ guess_source <- function(site) {
 
   if (nrow(source_tbl) > length(site)) {
     ambiguous_codes <- 
-      source_tbl |>
-      dplyr::add_count(.data$code) |>
-      dplyr::filter(.data$n > 1L) |>
-      dplyr::group_by(.data$code) |>
-      dplyr::summarise(source = paste(.data$source, collapse = ", ")) |>
-      dplyr::mutate(str = paste0(.data$code, " (", .data$source, ")")) |>
+      source_tbl %>%
+      dplyr::add_count(.data$code) %>%
+      dplyr::filter(.data$n > 1L) %>%
+      dplyr::group_by(.data$code) %>%
+      dplyr::summarise(source = paste(.data$source, collapse = ", ")) %>%
+      dplyr::mutate(str = paste0(.data$code, " (", .data$source, ")")) %>%
       dplyr::pull(.data$str)
 
     cli::cli_abort(
