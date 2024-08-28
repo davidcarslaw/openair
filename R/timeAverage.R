c888596b0a9011f37d214dbab011a03a3d8af0cf
marcelo oyaneder labarca <marcelo.oyaneder.l@gmail.com>
2024-08-28 19:14:02 -0400

    Update timeAverage.R

--- R/timeAverage.R
+++ R/timeAverage.R
@@ -244,7 +244,7 @@ timeAverage <- function(mydata, avg.time = "day", data.thresh = 0,
     FUN <-function(x){
       sorted_data <- sort(na.omit(x))
       n <- length(sorted_data)
-      position <- ceiling((percentile / 100) * n)
+      position <- ceiling((percentile) * n)
       return(sorted_data[position])
     }
   }
